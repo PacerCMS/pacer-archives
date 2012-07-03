515e97516bc18af2ee8b258a67d637ead64f5dc2
Stephen Yeargin <stephen.yeargin@gmail.com>
2012-07-02 20:02:43 -0500

    404 for articles that have bad URLs

--- controllers/article.php
+++ controllers/article.php
@@ -6,6 +6,12 @@ $Smarty->assign('article_id', $article_id);
 
 /* Article Data */
 $article = new Article($article_id);
+if (!$article->id):
+	header("HTTP/1.0 404 Not Found");
+	$Smarty->display('404.tpl');
+	exit;
+endif;
+
 $Smarty->assign('article', $article);
 $Smarty->assign('title', $article->title);
 $Smarty->assign('description', trim(str_replace(array("\n","\r",'  '), ' ', $article->summary)) );
